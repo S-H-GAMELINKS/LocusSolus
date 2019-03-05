bb2b0e0baee59a101efe05814e6f901ee6464ddf
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-05 23:25:30 +0900

    using bootstrap

--- assets/src/index.js
+++ assets/src/index.js
@@ -4,3 +4,6 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+import * as  Bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
