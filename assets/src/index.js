29c67f932ba94d236c95c561f1165172ae64fa99
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-06 12:35:57 +0900

    fix path for bootstrap-umi

--- assets/src/index.js
+++ assets/src/index.js
@@ -6,8 +6,8 @@ const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
-import * as  Bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
+import * as  Bootstrap from 'bootstrap-umi';
+import 'bootstrap-umi/dist/css/bootstrap.css';
 
 const app = new Vue({
     el: ".app"
