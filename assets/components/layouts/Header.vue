6fa95695a51b0a9552e74aac7d7b3ac1bb3505ae
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-06 12:40:35 +0900

    add Header.vue

---
+++ assets/components/layouts/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <router-link to="/" class="navbar-brand">Locus Solus</router-link>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <router-link to="/" class="dropdown-item">Top</router-link>
+                    <router-link to="/about" class="dropdown-item">About</router-link>
+                    <router-link to="/contact" class="dropdown-item">Contact</router-link>
+                    <router-link to="/chat" class="dropdown-item">Chat</router-link>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
