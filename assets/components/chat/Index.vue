98a2917984154216d5c6be5e2e5cc0571ea173e9
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-11 19:49:45 +0900

    create chat room title

--- assets/components/chat/Index.vue
+++ assets/components/chat/Index.vue
@@ -1,5 +1,30 @@
 <template>
     <div class="container">
-        <h1>Index Pagse</h1>
+        <h1>All Chat Room</h1>
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">ルーム名</span>
+            </div>
+            <input type="text" class="form-control" v-model="title" placeholder="ルーム名を入力してください"> 
+        </div>
+        <p>
+            <button type="button" class="btn btn-primary" v-on:click="createChatRoom">Create</button>
+        </p>
     </div>    
 </template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            title: "",
+        }
+    },
+    methods: {
+        createChatRoom: function() {
+            this.title = "";
+        }
+    }
+}
+</script>
+
