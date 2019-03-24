683e1ac907eb600e41ac5d19cf8294dd895cf8ae
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-24 23:00:46 +0900

    add GitHub Login button

--- assets/components/web/Index.vue
+++ assets/components/web/Index.vue
@@ -1,5 +1,45 @@
 <template>
+import { functions } from 'firebase';
     <div class="container">
         <h1>Index Pagse</h1>
+
+        <button v-if="!this.$store.state.session" v-on:click="login" class="btn btn-primary">Login to GitHub</button>
     </div>    
 </template>
+
+<script>
+
+export default {
+    mounted: function() {
+        this.userLogin();
+    },
+    methods: {
+        userLogin: function() {
+            const firebase = this.$store.state.firebase;
+            firebase.auth().onAuthStateChanged((response) => {
+                if (response) {
+                    this.$store.commit('getSession', true);
+                    router.push({path: '/'});
+                } else {
+                    this.$store.commit('getSession', false);
+                    router.push({path: '/'});
+                }
+            }, (error) => {
+                console.log(error);
+            });
+        },
+        login: function() {
+            const firebase = this.$store.state.firebase;
+            const provider = this.$store.state.provider;
+            firebase.auth().signInWithPopup(provider).then((response) => {
+                this.$store.commit('loginLocusSolus', response.user.uid);
+                this.$store.commit('setUserName', response.additionalUserInfo.profile.name);
+            }, (error) => {
+                console.log(error)
+            })
+            this.userLogin();
+        }
+    }
+}
+
+</script>
